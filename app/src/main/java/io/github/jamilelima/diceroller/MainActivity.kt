4cc059bec9edff22dab6d4e245864d7bf7d74a0c
jamilelima <jamilesousalima@gmail.com>
2019-01-01 21:12:17 -0300

    Add a reference for roll button on main activity

--- app/src/main/java/io/github/jamilelima/diceroller/MainActivity.kt
+++ app/src/main/java/io/github/jamilelima/diceroller/MainActivity.kt
@@ -2,11 +2,17 @@ package io.github.jamilelima.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
